111d81c794c46cf8c1cc6c9132287d779a751f77
Dusty DeWeese <dustin.deweese@gmail.com>
2022-08-19 13:51:43 -0700

    WIP #5721

--- crates/bevy_ui/src/render/render_pass.rs
+++ crates/bevy_ui/src/render/render_pass.rs
@@ -78,7 +78,7 @@ impl Node for UiPassNode {
         };
         let mut render_pass = render_context.begin_tracked_render_pass(RenderPassDescriptor {
             label: Some("ui_pass"),
-            color_attachments: &[Some(target.get_unsampled_color_attachment(Operations {
+            color_attachments: &[Some(target.get_color_attachment(Operations {
                 load: LoadOp::Load,
                 store: true,
             }))],

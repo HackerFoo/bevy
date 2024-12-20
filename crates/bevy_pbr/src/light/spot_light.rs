f1d2b29512584a7c46a267e136af06e7958e390a
Dusty DeWeese <dustin.deweese@gmail.com>
2024-12-19 17:53:23 -0800

    remove unnecessary Transform requirements

--- crates/bevy_pbr/src/light/spot_light.rs
+++ crates/bevy_pbr/src/light/spot_light.rs
@@ -9,7 +9,7 @@ use super::*;
 /// the transform, and can be specified with [`Transform::looking_at`](Transform::looking_at).
 #[derive(Component, Debug, Clone, Copy, Reflect)]
 #[reflect(Component, Default, Debug)]
-#[require(Frustum, VisibleMeshEntities, Transform, Visibility)]
+#[require(Frustum, VisibleMeshEntities, Visibility)]
 pub struct SpotLight {
     /// The color of the light.
     ///

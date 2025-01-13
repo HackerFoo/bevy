47c6f41eb77201d7017b5635240550368b68ea3e
Dusty DeWeese <dustin.deweese@gmail.com>
2025-01-13 13:15:20 -0800

    add messages

--- crates/bevy_winit/src/state.rs
+++ crates/bevy_winit/src/state.rs
@@ -842,6 +842,12 @@ impl<M: Message> WinitAppRunnerState<M> {
                 BevyWindowEvent::KeyboardFocusLost(e) => {
                     world.write_message(e);
                 }
+                BevyWindowEvent::MemoryWarning(e) => {
+                    world.send_event(e);
+                }
+                BevyWindowEvent::OpenFile(e) => {
+                    world.send_event(e);
+                }
             }
         }
 

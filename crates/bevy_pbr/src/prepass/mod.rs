8cc40cf203c6766df149b890e4a06bd4a2c471e5
Dusty DeWeese <dustin.deweese@gmail.com>
2026-04-17 21:29:13 -0700

    force prepass to use my fragment shader

--- crates/bevy_pbr/src/prepass/mod.rs
+++ crates/bevy_pbr/src/prepass/mod.rs
@@ -379,7 +379,7 @@ impl SpecializedMeshPipeline for PrepassPipelineSpecializer {
 }
 
 fn is_depth_only_opaque_prepass(mesh_key: MeshPipelineKey) -> bool {
-    mesh_key.intersection(MeshPipelineKey::ALL_PREPASS_BITS) == MeshPipelineKey::DEPTH_PREPASS
+    false
 }
 
 impl PrepassPipeline {
@@ -563,10 +563,9 @@ impl PrepassPipeline {
         // prepass shader, or we are emulating unclipped depth in the fragment shader.
         let fragment_required = !targets.is_empty()
             || emulate_unclipped_depth
-            || (mesh_key.contains(MeshPipelineKey::MAY_DISCARD)
-                && material_properties
-                    .get_shader(PrepassFragmentShader)
-                    .is_some());
+            || material_properties
+                .get_shader(PrepassFragmentShader)
+                .is_some();
 
         let fragment = fragment_required.then(|| {
             // Use the fragment shader from the material
@@ -1498,6 +1497,6 @@ pub type DrawDepthOnlyPrepass = (
     SetPrepassViewBindGroup<0>,
     SetPrepassViewEmptyBindGroup<1>,
     SetMeshBindGroup<2>,
-    SetPrepassEmptyMaterialBindGroup<3>,
+    SetMaterialBindGroup<3>,
     DrawMesh,
 );
